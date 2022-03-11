dbc6a371fedecd77dcad92ab39555c3350bf3818
Ryder Wendt <ryderw93@gmail.com>
2022-03-11 15:43:08 -0500

    swipe cards w/ disturbing aot pokemon images

--- tinder-clone/src/pages/Dashboard.js
+++ tinder-clone/src/pages/Dashboard.js
@@ -1,11 +1,64 @@
+import { useState } from "react";
+import TinderCard from 'react-tinder-card'
+
 const Dashboard = () => {
+  const [lastDirection, setLastDirection] = useState();
+  const characters = [
+    {
+      name: "Richard Hendricks",
+      url: "https://imgur.com/Uzc3s4j.jpg",
+    },
+    {
+      name: "Erlich Bachman",
+      url: "https://imgur.com/7MvY81t.jpg",
+    },
+    {
+      name: "Monica Hall",
+      url: "https://imgur.com/Fcy1vIF.jpg",
+    },
+    {
+      name: "Jared Dunn",
+      url: "https://imgur.com/tfilOFL.jpg",
+    },
+    {
+      name: "Dinesh Chugtai",
+      url: "https://imgur.com/3uUwLHc.jpg",
+    },
+  ];
+
+  const swiped = (direction, nameToDelete) => {
+    console.log("removing: " + nameToDelete);
+    setLastDirection(direction);
+  };
+
+  const outOfFrame = (name) => {
+    console.log(name + " left the screen!");
+  };
+
   return (
-    <div className='dashboard'>
-    {/* <ChatContainer/> */}
-
-
+    <div className="dashboard">
+      {/* <ChatContainer/> */}
+      <div className="swiper-container">
+        <div className="card-container">
+          {characters.map((character) =>
+            <TinderCard
+              className="swipe"
+              key={character.name}
+              onSwipe={(dir) => swiped(dir, character.name)}
+              onCardLeftScreen={() => outOfFrame(character.name)}
+            >
+              <div
+                style={{ backgroundImage: "url(" + character.url + ")" }}
+                className="card"
+              >
+                <h3>{character.name}</h3>
+              </div>
+            </TinderCard>
+          )}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
