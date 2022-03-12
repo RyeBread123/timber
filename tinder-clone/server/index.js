23c6f0d4609fa2516ee670b5d30a5dd87d731c36
Ryder Wendt <ryderw93@gmail.com>
2022-03-12 14:48:43 -0500

    added post signup route

--- tinder-clone/server/index.js
+++ tinder-clone/server/index.js
@@ -2,19 +2,62 @@ const express = require('express');
 const app = express();
 const PORT = 8000;
 const { MongoClient} = require('mongodb')
-
+const config = require('./config.js')
+// pick the version from uuid
+const { v4: uuidv4} = require('uuid')
 // connecting to cluster through mongoDB
-const uri = 'mongodb+srv://ryebread:bread@cluster0.sfnwn.mongodb.net/Cluster0?retryWrites=true&w=majority'
-
+const uri = `mongodb+srv://ryebread:${config.TOKEN}@cluster0.sfnwn.mongodb.net/Cluster0?retryWrites=true&w=majority`
+const jwt = require('jsonwebtoken')
+const cores = require('cors')
+app.use(cors())
 // --------------- REQUESTS ---------------------
 
 app.get('/', (req, res, next) => {
   res.json("Hello to my app")
 })
 
-app.post('/signup', (req, res, next) => {
+app.post('/signup', async (req, res, next) => {
   const client = new MongoClient(uri);
+  const {email, password} = req.body
+  // UUID will generate unique number/ID
+  cosnt generateduserId = uuidv4()
+  // returning a hashed password
+  const hashedPassword = await bcrypt.hash(password, 10)
 
+  try {
+    await client.connect()
+    const database = client.db('app-data')
+    // collection in mongo db we are connecting to
+    const users = database.collection('users')
+
+    // check if user exists
+    const existingUser = user.findOne({email})
+
+    if (existingUser) {
+      return res.send(409).send("user already exists. PLEASE login")
+    }
+    //sanitizing email
+    const sanitizedEmail = email.toLowerCase()
+
+    const data = {
+      // must match database
+      user_id: generatedUserId,
+      email: sanitizedEmail,
+      hashed_password: hashedPassword
+    }
+    const insertedUser = await users.insertOne(data)
+
+    // generate token
+    // can use a secret key but not being used here
+    // callback is amount of time to expire
+    const token = jwt.sign(insertedUser, sanitizedEmail, {
+      // expired in 24 hours
+      expiresIn: 60 * 24,
+    })
+    res.status(201).json({token, userId: generatedUserId, email: sanitizedEmail})
+  } catch (err) {
+    console.log(error)
+  }
 
 })
 
