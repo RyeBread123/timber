fb6bdab3088b222e1398083afa2e56a445c7fbdf
Ryder Wendt <ryderw93@gmail.com>
2022-03-12 13:56:15 -0500

    deleted

--- tinder-clone/Server/index.js
+++ tinder-clone/Server/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = 8000;
 const { MongoClient} = require('mongodb')
+
+// connecting to cluster through mongoDB
 const uri = 'mongodb+srv://ryebread:bread@cluster0.sfnwn.mongodb.net/Cluster0?retryWrites=true&w=majority'
+
+// --------------- REQUESTS ---------------------
 
 app.get('/', (req, res, next) => {
   res.json("Hello to my app")
@@ -14,13 +18,17 @@ app.post('/signup', (req, res, next) => {
 })
 
 app.get('/users', async (req, res, next) => {
+  // connects to uri
   const client = new MongoClient(uri)
 
   try {
     await client.connect()
+    // saving database as var
     const database = client.db('app-data')
+    // collection in mongo db we are connecting to
     const users = database.collection('users')
 
+    // querying collection
     const returnedUsers = await users.find().toArray()
     res.send(returnedUsers)
   } finally {
@@ -28,6 +36,8 @@ app.get('/users', async (req, res, next) => {
     await client.close()
   }
 })
+
+// ----------------------------LISTEN-----------------------------------------
 
 
 app.listen(PORT, () => {
